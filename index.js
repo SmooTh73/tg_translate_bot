3a7e17cf28d44081a15a623ec654355faba95113
SmooTh73 <vova.gladkiii@gmail.com>
2023-02-24 20:27:07 +0200

    index

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api';
 import constants from './src/config/constants.js';
-
+import { controller } from './src/controller/index.js';
 
 const bot = new TelegramBot(constants.telegram.HTTP_TOKEN, { polling: true });
 
-bot.on('message', async (message) => {
-    console.log(message);
-});
+bot.on('message', controller);
