4bfbc0ad7d0ec310d96bb947a44ab0fc857f2c67
SmooTh73 <vova.gladkiii@gmail.com>
2023-02-28 17:37:05 +0200

    USER | dto

---
+++ src/dtos/user.dto.js
@@ -0,0 +1,13 @@
+
+
+export default class UserDto {
+    username;
+    translate_from;
+    translate_to;
+
+    constructor(userObject) {
+        this.username = user.from.first_name,
+        this.translate_from = 'English'
+        this.translate_to = 'Ukrainian'
+    }
+}
